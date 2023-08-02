e62a515210c6f6cea510be7104c8f8163d7dd8ac
Ya2001 <srivats66@gmail.com>
2023-08-02 23:16:21 +0530

    changed contentful name

--- src/contentfulConfig/contentfulConfig.js
+++ src/contentfulConfig/contentfulConfig.js
@@ -11,7 +11,7 @@ export default client;
 export async function getImagesFromContentful(){
   try {
       const response = await client.getEntries({
-        content_type: 'madebyandrrew portfolio wesbite', 
+        content_type: 'madebyandrrew', 
       });
       console.log('Contentful Response:', response.items);
 
