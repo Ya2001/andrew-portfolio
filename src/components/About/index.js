b946d565ab73760124f12ccf06470d5141db3998
Ya2001 <srivats66@gmail.com>
2023-08-02 12:59:21 +0530

    added some stuff to the about page need to have it checked over

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,14 +7,16 @@ const About = () => {
     return (
     <div className='container'>
         <div className='profile-photo'>
-            <img img src={logo} alt='logo'></img>
+            <img img src= {logo} alt='logo'></img>
         </div>
         <div className='profile-text'>
+            <p>
             This is a block of text that will be displayed on the <br/>
             About page to talk about how cool andrew is and that hes a camera man with peas or something idk <br/>
             I need to write want this to be something short and succint but also gets to the points about <br/>
             your passions and stuff uno. Could also include some stuff on your influences unsure but fully about <br/>
             YOU!!
+            </p>
         </div>
     </div>
     )
